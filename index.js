2a16ed954095da9705ae938d584fb0f99819ecda
Tabithaokoth <kobbitho@gmail.com>
2024-05-12 19:45:24 +0300

    add changes

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const jsonServer = require('json-server');
+const cors = require('cors'); //import the CORS library
 
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = 5000;
+
+//Configure CORS to allow requests from any origin
+const corsOptions ={
+    origin: '*'
+};
+
+//Apply CORS middleware options
+server.use(cors(corsOptions));
+
+//Apply default middlewares
 server.use(middlewares);
 server.use(router);
-
 server.listen(port);
